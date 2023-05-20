1983f3bfd29888c066766516bba16fb679248ebb
nin.dev <ninn.tav@gmail.com>
2023-05-20 22:06:53 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(){
+        this.message = "hello world";
+        console.log(this.message);
+    }
+    getUser(){}
+    getUserStatus(){}
+}
+
+module.exports = User;
